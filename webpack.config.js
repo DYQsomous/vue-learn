6bc0c2fbe9a9ca4904408c6bd248176710ea07c7
Daisy Dai <893487287@qq.com>
2018-04-13 14:07:44 +0800

    Try to intergerat with webpack

---
+++ webpack.config.js
@@ -0,0 +1,49 @@
+const path = require('path');
+// const webpack = require('webpack');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const HtmlwebPackagePlugin = require('html-webpack-plugin');
+// const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+const ROOT_PATH = path.resolve(__dirname);
+const APP_PATH = path.resolve(ROOT_PATH, 'app');
+const BUILD_PATH = path.resolve(ROOT_PATH, 'build');
+
+module.exports = {
+  mode: 'development',
+  entry: {
+    app: path.resolve(APP_PATH, 'index.js'),
+    // vendors: ['vue']
+  },
+  // output: {
+  //   path: BUILD_PATH,
+  //   filename: '[name].js'
+  // },
+  devServer: {
+    historyApiFallback: true,
+    hot: true,
+    inline: true,
+    progress: true
+  },
+  // module: {
+  //   rules: [{
+  //     test: /\.sass$/,
+  //     exclude: /node_modules/,
+  //     use: ['styler-loader', 'css-loader', 'sass-loader']
+  //   }]
+  // },
+  plugins: [
+    new CleanWebpackPlugin(['build']),
+    new HtmlwebPackagePlugin ({
+      title: 'Hello Vue.js world!',
+      template: path.resolve(APP_PATH, 'index.html'),
+      filename: 'index.html',
+      // chunks: ['app', 'vendors']
+    }),
+  ],
+  // optimization: {
+  //   splitChunks: {
+  //     name: 'vendors',
+  //     filename: 'vendors.js'
+  //   },
+  // }
+}
