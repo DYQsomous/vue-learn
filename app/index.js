e365d1bc3cd09dab9412a2c4b325c974d3726bde
Daisy Dai <893487287@qq.com>
2018-04-16 14:54:46 +0800

    Over view about Vue.js

--- app/index.js
+++ app/index.js
@@ -1,1 +1,24 @@
-require('vue')
+new Vue({
+  el: '#app',
+  data: {
+    newTodo: '',
+    todos: [
+      {text: 'Learn Webpacke'},
+      {text: 'Learn vue'},
+      {text: 'Learn single page application'}
+    ]
+  },
+  methods: {
+    addTodo: function () {
+      var text = this.newTodo.trim();
+      console.log('text: ', text)
+      if (text) {
+        this.todos.push({ text: text });
+        this.newTodo = '';
+      }
+    },
+    removeTodo: function(index) {
+      this.todos.splice(index, 1)
+    }
+  }
+})
